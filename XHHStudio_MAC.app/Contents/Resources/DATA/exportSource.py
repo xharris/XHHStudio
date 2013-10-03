1ce7914bad1c2fdcbd61adb9e28c9b0cf52177a4
Xavier Harris <XHH@Xaviers-MacBook-Pro.local>
2013-10-03 12:13:49 -0400

    Mac (Lion) .app works but path management is way off
    why must osx be so different its a pain

---
+++ XHHStudio_MAC.app/Contents/Resources/DATA/exportSource.py
@@ -0,0 +1,223 @@
+def exportSrc(opersys='windows'): # build to which os?
+    global temp_path
+    dir = temp_path
+
+    if not os.path.exists(project_path):
+            os.makedirs(project_path)
+
+    # paths that are used in this process
+    struct_path = os.getcwd()+slash+'DATA'+slash+'love'+slash+'structure'+slash
+    temp_pathS = temp_path+slash
+
+    #copy images/sounds/etc
+    #if OS == 'mac':
+    #    if not os.path.exists(temp_path): os.makedirs(temp_path)
+    copyDir(project_path+slash+'LIBRARY_DATA',temp_path)
+    fontL,soundL = [],[]
+    if os.path.exists(temp_pathS+'FONTS'): fontL = os.listdir(temp_pathS+'FONTS')
+    if os.path.exists(temp_pathS+'SOUNDS'): soundL = os.listdir(temp_pathS+'SOUNDS')
+
+    # copy the main.lua template
+    shutil.copy(struct_path+'main.lua',temp_path)
+
+    # copy config file and change settings
+    shutil.copy(struct_path+'conf.lua',temp_path)
+    f = open(temp_pathS+'conf.lua','r')
+    cStr = f.read()
+    f.close()
+    f = open(temp_pathS+'conf.lua','wb')
+    for s in lobjects['settings']:
+        if isinstance(s[2],basestring):
+            cStr = cStr.replace(s[1].lower()+' = ',s[1].lower()+' = "'+str(s[2])+'"')
+        else:
+            cStr = cStr.replace(s[1].lower()+' = ',s[1].lower()+' = '+str(s[2]).lower())
+
+    f.write(cStr)
+    f.close()
+
+
+    # add requires for all lobjects and write code
+    requireStr = 'require("32log")\n'
+    shutil.copy(struct_path+'32log.lua',temp_path)
+
+    # add "plugins"
+    copyDir(struct_path+'PLUGINS',temp_pathS+'PLUGINS') # my personal API
+    copyDir(struct_path+'LIB_IMG',temp_pathS+'LIB_IMG')
+
+    for d in os.listdir(temp_pathS+'PLUGINS'):
+        if '.lua' in d:
+            d = d.replace('.lua','')
+        requireStr += 'require("PLUGINS/'+d+'")\n'
+
+
+    # add my personal API
+    copyDir(struct_path+'API',temp_pathS+'API') 
+    for d in os.listdir(struct_path+'API'):
+        d = d.replace('.lua','')
+        d = d.replace('~','')
+        if d == 'EFFECTS':
+            requireStr += 'if love.graphics.isSupported("pixeleffect") then require("API/'+d+'") end\n'
+        else:
+            requireStr += 'require("API/'+d+'")\n'
+
+    other_requires = ['math','game','custom','start','main_extra']
+
+    for o in other_requires:
+            p = struct_path+o+'.lua'
+            if os.path.isfile(p):
+                requireStr += 'require("'+o+'")\n'
+                shutil.copy(p,temp_path)
+
+    folder = dir+slash
+    requireF = open(temp_pathS+'requires.lua','wb')
+
+    # rewrite main_extra
+    mef = open(temp_pathS+'main_extra.lua','wb')
+    mef.write(str(lobjects['main']))
+    mef.close()
+
+    # get the template object lua file
+    templateF = open(struct_path+'object.lua','r')
+    templateS = templateF.read()
+
+    templateF.close()
+
+    for index,o in enumerate(lobjects['object']):
+        o = str(o)
+        name = lobjects['object'][o]['name']
+        requireStr += 'require("'+name+'")\n'
+        
+        # write lobject code
+        lobj = lobjects['object'][o]
+
+        template = templateS+'\n\n'
+
+        ##startCode = lobj['code']['onCreate'].replace('"','\\\"')
+        animations = ''
+        hitboxes = ''
+        
+        for i,a in enumerate(lobjects['object'][o]['images']):
+            animations += '{["img"]=GFX.newAnimation(GFX.newImage("'+str(a['path'])+'"),'+str(a['width'])+','+str(a['height'])+','+str(a['speed'])+','+str(a['frames'])+'),["width"]='+str(a['width'])+',["height"]='+str(a['height'])+',["speed"]='+str(a['speed'])+',["frames"]='+str(a['frames'])+'},\n'
+            
+            if a['hitbox'] == 0:
+                hitboxes += 'love.physics.newRectangleShape('+str(a['width'])+','+str(a['height'])+')}'
+            else:
+                for h in a['hitbox']:
+                    hitboxes += 'love.physics.newPolygonShape('
+                    for pts in h:
+                        print pts
+                        hitboxes += str(pts[0])+','+str(pts[1])+','
+                    hitboxes = hitboxes[:-1]+'),' 
+        hitboxes = hitboxes[:-1]
+        animations = animations[:-2]
+
+        a = lobjects['object'][o]['images'][0]
+        firstAnimation = a['path']    
+
+        add = [         ['self.animation=""','self.animation="'+firstAnimation+'"'],
+                        ['self.FRAMES=0','self.FRAMES='+str(a['frames'])],
+                        ['self.anim=newAnimation()','self.anim=newAnimation(GFX.newImage("'+str(a['path'])+'"),'+str(a['width'])+','+str(a['height'])+','+str(a['speed'])+','+str(a['frames'])+')'],
+                        ['self.img_speed=0','self.img_speed='+str(a['speed'])],
+                        ]
+
+        replacements = [['HITBOXES={}','HITBOXES={'+hitboxes+'}'],
+                        ['Object',name],
+                        ['ANIMATIONS={}','ANIMATIONS={'+animations+'}'],
+                        ['z=0','z='+str(lobj['depth'])]
+                        ]
+                        
+
+        for r in replacements:
+            template = template.replace(r[0],r[1])
+
+        # update/draw/create/etc code typed in the editor
+
+        for code in ['onCreate',    
+                     'onUpdate',    
+                     'onDraw', 
+                     'onDestroy',     
+                     'onKeyRelease',
+                     'onKeyPress',  
+                     'onMouseRelease',
+                     'onMousePress',
+                     'onCollisionStart',
+                     'onColliding', 
+                     'onCollisionEnd']:
+
+            functempF = open(struct_path+'object'+slash+code+'.txt','r')
+            functemp = functempF.read()
+            functempF.close()
+            functemp = functemp.replace('Object',name)
+
+            if code in lobj['code']:
+                if not code=='onDraw' and not code=='onCreate':
+                    template += functemp+'\n'
+                    template += lobj['code'][code]
+                elif code == 'onCreate':
+                    template += functemp+'\n'
+                    for a in add:
+                        template += a[1]+'\n'
+                    template+=lobj['code'][code]+'\naddObj(self)\n'
+                elif code == 'onDraw':
+                    if len(lobj['code']['onDraw'])>2:
+                        template += 'function '+name+':onDraw()\n'+lobj['code']['onDraw']
+                    else:
+                        template += functemp+'\n'
+                template += '\nend\n\n'
+            else:
+                template += functemp+'\n'
+                template += '\nend\n\n'
+
+        obj_file = open(temp_pathS+name+'.lua','wb')
+        
+        o_file = open(temp_pathS+name+'.lua','wb')
+        obj_file.write(template)
+        obj_file.close()
+
+    # entities in each state    
+    for i,s in enumerate(lobjects['state']):
+        stateF = open(temp_pathS+'state'+str(i)+'.lua','wb')
+        requireStr += 'require("state'+str(i)+'")\n'
+        stateCode = 'function state'+str(i)+'start()\nstateName="'+lobjects['state'][s]['name']+'"\n'
+        for e in lobjects['state']['state'+str(i)]['entities']:
+            if hasattr(e,'kind') and e.kind == 'entity' and not e.destroying:
+                stateCode += 'local self='+e.name+':new('
+                stateCode += repr(e.save['x'])+','
+                stateCode += repr(ORIGIN[1]-e.save['y'])+')\n'
+                stateCode += 'self.state = '+str(e.save['state'])+'\n'
+                if e.save['img_index'] != 0: stateCode += 'self.img_speed = 0\n'
+                stateCode += 'self.img_frame = '+str(e.save['img_index']+1)+'\n'
+                stateCode += 'self.animation_index = '+str(e.save['img_ani']+1)+'\n'
+                stateCode += 'self.img_angle = '+str(e.save['img_angle'])+'\n'
+                stateCode += e.save['code']+'\nself=nil\n'
+                
+                
+        stateCode += '\n'+lobjects['state'][s]['code']+'\nend\n'
+        stateF.write(stateCode)
+        stateF.close()
+
+    statesS = 'STATES={ '
+    for s in lobjects['state']:
+        statesS += '"'+lobjects['state'][s]['name']+'",'
+    statesS = statesS[:-1]+'}\n'
+    requireStr += statesS
+
+    fontS = 'FONTS={ '
+    for f in fontL:
+        fontS += '"FONT/'+f+'",'
+    fontS = fontS[:-1]+'}\n'
+    requireStr += fontS
+
+    soundS = 'SOUNDS={ '
+    for s in soundL:
+        soundS += '["'+s+'"]={["volume"]=1,["pitch"]=1},'#'["'+s+'"]=love.audio.newSource("SOUNDS/'+s+'","stream"),'
+    soundS = soundS[:-1]+'}\n'
+    requireStr += soundS
+
+    requireF.write(requireStr)
+    requireF.close()
+
+        # add custom user events for main loop
+        #f = open(folder+'custom.lua')
+
+exportSrc(OS)
---
+++ XHHStudio_MAC.app/Contents/Resources/DATA/love/structure/main.lua
@@ -0,0 +1,264 @@
+require('requires')
+
+STATE_INDEX = 0
+STATE=''
+
+backgroundColor = {255,255,255,255}
+
+function love.load()
+	if notNil(main_preLoad) then main_preLoad() end
+	loaded = false
+	--love.physics.setMeter(64)
+    	phys_world:setCallbacks(onCollisionStart, onCollisionEnd, onColliding, postSolve)
+        --phys_world:setContactFilter(contFilter)
+    	phys_world:setGravity(0,0)
+    	persisting = 0
+    	love.graphics.setDefaultImageFilter("nearest", "nearest")  -- antialiasing
+	loadstring('state0start()')()
+	loaded = true
+	if notNil(main_postLoad) then main_postLoad() end
+end
+
+function love.update(dt)
+	if notNil(main_preUpdate) then main_preUpdate(dt) end
+	love.timer.sleep(.001)
+	for k,d in ipairs(objects) do
+		if notNil(d.update) and loaded then
+			d:update(dt)
+		end
+	end
+    if notNil(EFFECTS) then
+        for k,d in ipairs(EFFECTS) do
+            print('heres '..d)
+            if d.enabled then
+                print('ok oks')
+                d:update(dt)
+            end
+        end
+    end
+    
+	phys_world:update(dt)
+	if notNil(main_postUpdate) then main_postUpdate(dt) end
+end
+
+function love.draw(dt)
+	if notNil(main_preDraw) then main_preDraw(dt) end
+	if loaded then
+    	love.graphics.reset()
+                CAM.set()
+		for k,d in ipairs(objects) do
+			love.graphics.setBackgroundColor(backgroundColor)
+			if DBG.visible then DBG:draw() end
+			if notNil(d.onDraw) then
+					d:onDraw()
+			end
+                        
+            
+		end
+        --PXL:clear()
+    CAM.unset()
+	end
+	if notNil(main_postDraw) then main_postDraw() end
+end
+
+function love.keypressed(key)
+	if notNil(main_preKeyPressed) then main_preKeyPressed(key) end
+	--if key == "p" then --toggle pause
+	--	paused = not paused
+	--	print("paused: ",paused)
+	--end
+	if key == "f11" then
+		love.graphics.toggleFullscreen()
+		fullscreen = not fullscreen
+		--iniWrite("fullscreen",tostring(fullscreen))
+	end
+	if key == "escape" then
+		love.event.quit()
+	end
+	for k,d in ipairs(objects) do
+		if notNil(d.keypress) then
+			d:keypress(key)
+		end
+	end
+	if notNil(main_postKeyPressed) then main_postKeyPressed(key) end
+end
+
+function love.keyreleased(key)
+	if notNil(main_preKeyReleased) then main_preKeyReleased(key) end
+	--if key == "p" then --toggle pause
+	--	paused = not paused
+	--	print("paused: ",paused)
+	--end
+	for k,d in ipairs(objects) do
+		if notNil(d.keyrelease) then
+			d:keyrelease(key)
+		end
+	end
+	if notNil(main_postKeyReleased) then main_postKeyReleased(key) end
+end
+
+function love.mousepressed(x,y,button)
+	if notNil(main_preMousePressed) then main_preMousePressed(x,y,button) end
+    for k,d in ipairs(objects) do
+        if notNil(d.mousepress) then
+            d:mousepress(x,y,button)
+        end
+    end
+	if notNil(main_postMousePressed) then main_postMousePressed(x,y,button) end
+end
+
+function love.mousereleased(x,y,button)
+	if notNil(main_preMouseReleased) then main_preMouseReleased(x,y,button) end
+    for k,d in ipairs(objects) do
+        if notNil(d.mouserelease) then
+            d:mouserelease(x,y,button)
+        end
+    end
+	if notNil(main_postMouseReleased) then main_postMouseReleased(x,y,button) end
+end
+
+function love.quit()
+	if notNil(main_preQuit) then main_preQuit() end
+    if notNil(main_postQuit) then main_postQuit() end
+end
+
+-- Collision functions
+function onCollisionStart(a, b, coll)
+	    coll:setEnabled(false)
+	    x,y = coll:getNormal()
+	    a = a:getUserData()
+	    b = b:getUserData()
+	    v1 = ''
+	    v2 = ''
+	    for i,v in ipairs(objects) do
+	    	if v.id == a['id'] then v1 = v end
+	    	if v.id == b['id'] then v2 = v end
+	    end
+		if not v2 == nil then v1:collision(v2) end
+		if not v1 == nil then v2:collision(v1) end
+    --DBG.print(a['name'].." started collision with "..b['name'].." with a vector normal of: "..x..", "..y)
+end
+
+function onCollisionEnd(a, b, coll)
+	    coll:setEnabled(false)
+	    a = a:getUserData()
+	    b = b:getUserData()
+	    v1 = nil
+	    v2 = nil
+	    for i,v in ipairs(objects) do
+	    	if v.id == a['id'] then v1 = v end
+	    	if v.id == b['id'] then v2 = v end
+	    end
+	    if not v2 == nil then v1:collisionEnd(v2) end
+	    if not v1 == nil then v2:collisionEnd(v1) end
+    --DBG.print(a['name'].." uncolliding with "..b['name'])
+end
+
+function onColliding(a, b, coll)
+	    coll:setEnabled(false)
+		a = a:getUserData()
+	    b = b:getUserData()
+	    v1 = nil
+	    v2 = nil
+	    for i,v in ipairs(objects) do
+	    	if v.id == a['id'] then v1 = v end
+	    	if v.id == b['id'] then v2 = v end
+	    end
+		if not v2 == nil then v1:inCollision(v2) end
+	    if not v1 == nil then v2:inCollision(v1) end
+    --DBG.print(a['name'].." in a collision with "..b['name'])
+end
+	
+function postSolve(a, b, coll)
+end
+
+function contFilter(a,b)
+    a = a:getUserData()
+    b = b:getUserData()
+    v1 = nil
+    v2 = nil
+    for i,v in ipairs(objects) do
+        if v.id == a['id'] then v1 = v end
+        if v.id == b['id'] then v2 = v end
+    end
+    DBG.print(v1.name..v2.name)
+    return True
+end
+
+--[[
+local function error_printer(msg, layer)
+    print((debug.traceback("Error: " .. tostring(msg), 1+(layer or 1)):gsub("\n[^\n]+$", "")))
+end
+
+function love.errhand(msg)
+    msg = tostring(msg)
+
+    error_printer(msg, 2)
+
+    if not love.window or not love.graphics or not love.event then
+        return
+    end
+
+    if not love.graphics.isCreated() or not love.window.isCreated() then
+        if not pcall(love.window.setMode, 800, 600) then
+            return
+        end
+    end
+
+    -- Load.
+    if love.audio then love.audio.stop() end
+    love.graphics.reset()
+    love.graphics.setBackgroundColor(89, 157, 220)
+    local font = love.graphics.newFont(14)
+    love.graphics.setFont(font)
+
+    love.graphics.setColor(255, 255, 255, 255)
+
+    local trace = debug.traceback()
+
+    love.graphics.clear()
+    love.graphics.origin()
+
+    local err = {}
+
+    table.insert(err, "Error\n")
+    table.insert(err, msg.."\n\n")
+
+    for l in string.gmatch(trace, "(.-)\n") do
+        if not string.match(l, "boot.lua") then
+            l = string.gsub(l, "stack traceback:", "Traceback\n")
+            table.insert(err, l)
+        end
+    end
+
+    local p = table.concat(err, "\n")
+
+    p = string.gsub(p, "\t", "")
+    p = string.gsub(p, "%[string \"(.-)\"%]", "%1")
+
+    local function draw()
+        love.graphics.clear()
+        love.graphics.printf(p, 70, 70, love.graphics.getWidth() - 70)
+        love.graphics.present()
+    end
+
+    while true do
+        love.event.pump()
+
+        for e, a, b, c in love.event.poll() do
+            if e == "quit" then
+                return
+            end
+            if e == "keypressed" and a == "escape" then
+                return
+            end
+        end
+
+        draw()
+
+        if love.timer then
+            love.timer.sleep(0.1)
+        end
+    end
+end
+]]--
